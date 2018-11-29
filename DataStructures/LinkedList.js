917f76b22d49a99595fc4f318002fdb442986799
Kevin Turkington <turkingtonkevin@gmail.com>
2018-11-28 22:21:54 -0800

    reverse linked list

--- DataStructures/LinkedList.js
+++ DataStructures/LinkedList.js
@@ -91,3 +91,18 @@ export class LinkedList {
     }
   }
 }
+
+
+function reverseList(head) {
+  let prev = null, curr = head
+  
+  while (curr !== null) {
+    let temp = curr.next
+    curr.next = prev
+
+    prev = curr  
+    curr = temp
+  }
+
+  return prev
+}
