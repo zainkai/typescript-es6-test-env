627a1f7e49d7b432807773677018b16b700fed15
Kevin Turkington <turkingtonkevin@gmail.com>
2018-12-09 01:14:00 -0800

    package lock update and graph search query

---
+++ graphs/search.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @param {[Edge[start:any,end:any,val:Number]]} graph 
+ * @param {[start,end]} query
+ */
+function dfs(graph, query) {
+  const [qStart, qEnd] = query
+  let map = {
+    [qStart]: 0
+  }
+
+  let arr = [qStart]
+  while(arr.length !== 0) {
+    let curr = arr.pop() // <--- using stack
+
+    graph.filter(t => t[0] === curr).forEach(edge => {
+      const [u,v,w] = edge
+      if(!arr.includes(v)) {
+        arr.push(v)
+      }
+
+      map[v] = Math.min(map[v] || Infinity, map[u] + w)
+    })
+  }
+  return map[qEnd] || -1
+}
+
+console.log(dfs(
+  [[2,1,1],[2,3,1],[1,4,1],[3,4,3]],
+  [2,4]
+))
